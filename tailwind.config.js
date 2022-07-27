76616b89eb8d80d92be6349b0dcbc204383dd4b1
hito-kotaro <tohi.work@gmail.com>
2022-07-28 08:19:46 +0900

    add extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   content: [],
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: '#F1F1F1',
+        link: '#4B72E0',
+        primary: '#00C800',
+        secondary: '#9A9B9C',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+    },
   },
   plugins: [],
 };
