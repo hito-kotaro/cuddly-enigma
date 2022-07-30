a9094830dbc5732d0c4d2a83d2ab8729a360fd40
hito-kotaro <tohi.work@gmail.com>
2022-07-31 07:34:20 +0900

    add material ui

--- src/components/pages/HomePage/templates/HomeTemplate.tsx
+++ src/components/pages/HomePage/templates/HomeTemplate.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import useLogin from '../../../../hooks/useLogin';
-import Button from '../../../atoms/Button/Button';
 
 const HomeTemplate = () => {
   const { logout } = useLogin();
+
+  const dummy = () => {
+    toast.success('welcome');
+  };
+
   return (
-    <Button onClick={logout} isEnable>
-      logout
-    </Button>
+    <>
+      <Button onClick={logout}>Primary</Button>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <ListItem alignItems="flex-start" onClick={dummy}>
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="洗濯物干し"
+            secondary="おさらあらいありがとう"
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="洗濯物干し"
+            secondary="おさらあらいありがとう"
+          />
+        </ListItem>
+      </List>
+    </>
   );
 };
 
