1482a5af4d88f5269d4c62c4ccafeacce5dee2a1
hito-kotaro <tohi.work@gmail.com>
2022-08-01 08:51:53 +0900

    :put_litter_in_its_place:

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import useUserAgentState from '../../../stores/UserAgentState/useUserAgentState';
 import HomeTemplate from './templates/HomeTemplate';
 
 const HomePage = () => {
+  const { isSafari } = useUserAgentState();
   return (
-    <>
+    <div className={`bg-red-200 ${isSafari}`}>
       <HomeTemplate />
-    </>
+    </div>
   );
 };
 
