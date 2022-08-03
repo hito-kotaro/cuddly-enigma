ffff56dce9509edf35e06b3c9c0e5c198f504f51
hito-kotaro <tohi.work@gmail.com>
2022-08-03 20:54:00 +0900

    add template

--- src/components/atoms/Input/hooks/useInputNumber.ts
+++ src/components/atoms/Input/hooks/useInputNumber.ts
@@ -5,7 +5,7 @@ const useInputNumber = () => {
 
   const onChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const re = /^[0-9\b]+$/;
+      const re = /^[0-9\b]+$|\./;
       if (e.target.value === '' || re.test(e.target.value)) {
         setValueNum(e.target.value);
       }
--- src/components/pages/HomePage/templates/RequestTemplate.tsx
+++ src/components/pages/HomePage/templates/RequestTemplate.tsx
@@ -1,10 +1,110 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, useEffect } from 'react';
+import { Autocomplete, Button, Grid, TextField } from '@mui/material';
+import useInput from '../../../atoms/Input/hooks/useInput';
+import useInputNumber from '../../../atoms/Input/hooks/useInputNumber';
 
 const RequestTemplate = () => {
+  type userOptionType = { label: String; id: number };
+  const rewardInputHandler = useInputNumber();
+  const titleInputHandler = useInput();
+  const descInputHandler = useInput();
+  const [val, setVal] = useState<userOptionType>({ label: '', id: -1 });
+  const [isDisable, setIsDisable] = useState(false);
+
+  const top100Films = [
+    { label: 'Public', id: 0 },
+    { label: 'User1', id: 1 },
+    { label: 'User2', id: 2 },
+    { label: 'User3', id: 3 },
+  ];
+
+  const onChange = (value: { label: string; id: number }) => {
+    setVal(value);
+  };
+
+  const testConsole = () => {
+    const newRequest = {
+      title: titleInputHandler.value,
+      reward: rewardInputHandler.valueNum,
+      order: val.id,
+    };
+    console.log(newRequest);
+  };
+
+  const validateInput = () => {
+    if (
+      titleInputHandler.value === '' ||
+      rewardInputHandler.valueNum === '' ||
+      val.id === -1
+    ) {
+      setIsDisable(true);
+    } else {
+      setIsDisable(false);
+    }
+  };
+
+  useEffect(() => {
+    validateInput();
+  }, [rewardInputHandler.valueNum, titleInputHandler.value, val]);
+
   return (
-    <>
-      <div>RequestTemplate</div>
-    </>
+    <div className="px-2">
+      <div className="h-5" />
+
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <TextField
+              fullWidth
+              label="Title"
+              variant="outlined"
+              onChange={titleInputHandler.onChange}
+              value={titleInputHandler.value}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              fullWidth
+              label="Reward"
+              variant="outlined"
+              onChange={rewardInputHandler.onChange}
+              value={rewardInputHandler.valueNum}
+            />
+          </Grid>
+        </Grid>
+      </div>
+
+      <div className="h-5" />
+
+      <Autocomplete
+        disablePortal
+        options={top100Films}
+        isOptionEqualToValue={(option, value) => option.id === value.id}
+        onChange={(e, v) => onChange(v ?? { label: '', id: -1 })}
+        renderInput={(params) => (
+          <TextField {...params} fullWidth label="User" />
+        )}
+      />
+
+      <div className="h-5" />
+
+      <TextField
+        label="Comment"
+        fullWidth
+        multiline
+        minRows={10}
+        maxRows={10}
+        value={descInputHandler.value}
+        onChange={descInputHandler.onChange}
+      />
+      <div className="h-5" />
+      <div className="flex justify-center w-11/12 mx-auto">
+        <Button onClick={testConsole} variant="contained" disabled={isDisable}>
+          依頼発行
+        </Button>
+      </div>
+    </div>
   );
 };
 
