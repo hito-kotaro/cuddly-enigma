5967e123cd0b7619ac238d4940e2db1c9b3008d7
hito-kotaro <tohi.work@gmail.com>
2022-08-11 14:27:04 +0900

    add gas display

--- src/components/organisms/HomeHeader/HomeHeader.tsx
+++ src/components/organisms/HomeHeader/HomeHeader.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import useGasState from '../../../stores/GasState/useGasState';
 import HeaderLogo from '../../atoms/HeaderLogo/HeaderLogo';
 import MenuButton from '../../atoms/MenuButton/MenuButton';
 
 const HomeHeader = () => {
+  const { gas } = useGasState();
   return (
     <div className="sticky top-0 z-40 bg-base flex p-1">
       <div>
         <HeaderLogo />
+      </div>
+      <div className="ml-auto leading-10">
+        現在のGas:
+        <span className="font-semibold text-red-500">{gas * 100}%</span>
       </div>
       <div className="ml-auto">
         <MenuButton />
