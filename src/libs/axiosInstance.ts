f31b789fc1e60d410dcecf0bf1875bb57002041a
hito-kotaro <tohi.work@gmail.com>
2022-08-11 15:42:57 +0900

    change api url

--- src/libs/axiosInstance.ts
+++ src/libs/axiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.12:8000/',
+  // baseURL: 'http://192.168.0.12:8000/',
   // baseURL: 'http://localhost:8000/',
+  baseURL: 'https://hanamaru-hub.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -10,8 +11,9 @@ export const axiosInstance = axios.create({
 
 export const createAxiosTokenInstance = () => {
   const axiosTokenInstance = axios.create({
-    baseURL: 'http://192.168.0.12:8000/',
+    // baseURL: 'http://192.168.0.12:8000/',
     // baseURL: 'http://localhost:8000/',
+    baseURL: 'https://hanamaru-hub.herokuapp.com/',
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
       'Content-Type': 'application/json',
