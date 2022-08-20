44721460102706910f97df4d3c2fcca0bdab94fb
hito-kotaro <tohi.work@gmail.com>
2022-08-20 09:41:40 +0900

    wip: add user name change modal

--- src/components/pages/HomePage/templates/UserUpdateTemplate.tsx
+++ src/components/pages/HomePage/templates/UserUpdateTemplate.tsx
@@ -1,14 +1,5 @@
-import React, { useState } from 'react';
-import {
-  Avatar,
-  Box,
-  Button,
-  Input,
-  Modal,
-  TextField,
-  Typography,
-} from '@mui/material';
-import { BiEdit } from 'react-icons/bi';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Box, Button, Grid, Modal, TextField } from '@mui/material';
 import stringToColor from '../../../../libs/stringToColor';
 import useInput from '../../../atoms/Input/hooks/useInput';
 import useUserState from '../../../../stores/UserState/useUserState';
@@ -17,6 +8,7 @@ import LinkButton from '../../../atoms/LinkButton/LinkButton';
 const UserUpdateTemplate = () => {
   const { user } = useUserState();
   const [isModal, setIsModel] = useState(false);
+  const [isDisable, setIsDisable] = useState(true);
   const nameInputHandler = useInput(user.name);
   const currentPwdHandler = useInput();
   const newPwdHandler = useInput();
@@ -30,7 +22,7 @@ const UserUpdateTemplate = () => {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    // border: '2px solid #000',
     boxShadow: 24,
     p: 4,
   };
@@ -43,6 +35,23 @@ const UserUpdateTemplate = () => {
   const handleOpen = () => {
     setIsModel(true);
   };
+
+  const validateInput = () => {
+    // 特殊文字チェックは後で
+    if (
+      nameInputHandler.value === '' ||
+      nameInputHandler.value === user.name ||
+      nameInputHandler.value.length > 20
+    ) {
+      setIsDisable(true);
+    } else {
+      setIsDisable(false);
+    }
+  };
+
+  useEffect(() => {
+    validateInput();
+  }, [nameInputHandler]);
 
   return (
     <div>
@@ -62,9 +71,24 @@ const UserUpdateTemplate = () => {
             onChange={nameInputHandler.onChange}
             value={nameInputHandler.value}
           />
-          <Button onClick={() => console.log(nameInputHandler.value)}>
-            change
-          </Button>
+
+          <div className="h-5" />
+          <div className="flex justify-around">
+            <div className="text-center">
+              <Button variant="contained" onClick={handleClose}>
+                キャンセル
+              </Button>
+            </div>
+            <div className="text-center">
+              <Button
+                disabled={isDisable}
+                variant="contained"
+                onClick={() => console.log(nameInputHandler.value)}
+              >
+                変更
+              </Button>
+            </div>
+          </div>
         </Box>
       </Modal>
       <div className="h-10" />
@@ -80,7 +104,16 @@ const UserUpdateTemplate = () => {
         <div className="text-xl">{user.name}</div>
       </div>
 
-      <LinkButton onClick={handleOpen}>ユーザー名を変更</LinkButton>
+      <div className="h-5" />
+      <div className="flex justify-center">
+        <LinkButton onClick={handleOpen}>ユーザー名を変更</LinkButton>
+      </div>
+
+      <div className="h-5" />
+
+      <div className="flex justify-center">
+        <LinkButton onClick={handleOpen}>パスワードを変更</LinkButton>
+      </div>
     </div>
   );
 };
